caa5bf21c471bf83cc0ee0eb0102a26fb2735559
nabilislam11 <hamjanabil11@gmail.com>
2026-03-25 15:47:24 +0600

    sidebar added

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import logo from "../../assets/logo.png"  
 import { VscThreeBars } from 'react-icons/vsc'
 import { BsThreeDots } from 'react-icons/bs'
+import { CiSearch } from 'react-icons/ci'
+import { MdShoppingCart } from "react-icons/md"
+import { FaSms } from "react-icons/fa"
+import { IoIosNotifications } from "react-icons/io"
+import vactor from '../../assets/vactor.svg'
 const Navbar = () => {
   return (
     <div className='py-4  px-7 bg-[#615DFA]  flex justify-between items-center gap-[73px] ' >
-      <div className="w[45%] flex justify-between  items-center gap-[174px] ">
+      <div className="w-[35%] flex justify-between  items-center gap-[174px] ">
         <div className="flex items-center gap-3">
 
         <div className=" ">
@@ -22,11 +26,42 @@ const Navbar = () => {
         </div>
 
       </div>
-      <div className=" w-[55%] flex justify-between items-center">
-        <div className="w-[45%] ">
-          <input className='py-[15px] px-[16px]' type="text"  placeholder='Search here ....'/>
+      <div className=" w-[55%] flex justify-between items-center gap-20">
+        <div className="w-[40%] ">
+       <div className="relative w-[300px]">
+  <input
+    type="text"
+    placeholder="Search here..."
+    className="w-full py-2 pl-4 pr-10 rounded-md bg-[#4E4AC8] text-white placeholder-[#6965E0] outline-none"
+  />
+
+  {/* Search Icon */}
+  <span className="absolute right-3 top-1/2 -translate-y-1/2 text-[#6763DF]">
+  <CiSearch />
+    
+  </span>
+</div>
+
         </div>
-        <div className="w-[55%]"></div> 
+        <div className="w-[55%] flex items-center justify-between gap-10">
+          <div className="w-[25%]">
+            <div className="flex justify-between items-center gap-11 ">
+              <p className="font-poppins text-[12px] font-normal ">Next</p>
+              <p className="font-poppins text-[12px] font-normal">35 EXP</p>
+            </div>
+            <div className=" relative  h-[3px] rounded-lg w-full bg-[#4E4AC8] after:content-['']  after:absolute after:h-[3px] after:rounded-lg after:w-[50%] after:bg-[#41EFFF]  "></div>
+
+          </div>
+          <div className="w-[30] flex justify-between items-center gap-8 ">
+            <div className="w-[1px]  h-[32px] bg-[#7A77FD] "></div>
+
+<MdShoppingCart className="text-[#7A77FD]" />
+<FaSms className="text-[#7A77FD]" />
+<IoIosNotifications className="text-[#7A77FD]" />
+ <div className="w-[1px] h-[32px] bg-[#7A77FD] "></div>
+<img src={vactor} alt="" />
+          </div>
+          </div> 
       </div>
     </div>
   )
--- src/pages/Sidebar.tsx
+++ src/pages/Sidebar.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
 
+import { FaNewspaper } from 'react-icons/fa'
+import newsfeed from '../assets/newspaper.svg'
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <>
+    <div className=' '>
+      <div className="relative  w-full h-[75px]  bg-[#45437F]"></div>
+      <div className=" absolute top-[100px] left-[7.5%]    rounded-full w-[120px] h-[120px] border-4 border-white bg-[#615DFA] "></div>
+     <div className=" pl-[24px] pr-[8px] ">
+       <div className="pt-[75px] text-center ">
+        <p className='font-poppins font-bold text-[16px] text-[#000000]'>A B M Shawon Islam</p>
+        <p className='font-poppins font-medium text-[12px] 
+        text-[#AFB0C0]'>www.abm.com</p>
+      </div>
+      <div className="pt-[42px] flex items-center justify-center text-center gap-6 ">
+        <div className="">
+          <p className='font-poppins font-bold text-[16px] text-[#000000]'>930</p>
+          <p className='font-poppins font-medium text-[12px] 
+        text-[#AFB0C0]'>Friends</p>
+        </div>
+        <div className="w-[1px] h-[25px] bg-[#AFB0C0] "></div>
+        <div className="">
+          <p className='font-poppins font-bold text-[16px] text-[#000000]'>930</p>
+          <p className='font-poppins font-medium text-[12px] 
+        text-[#AFB0C0]'>Friends</p>
+        </div>
+        <div className="w-[1px] h-[25px] bg-[#AFB0C0] "></div>
+        <div className="">
+          <p className='font-poppins font-bold text-[16px] text-[#000000]'>930</p>
+          <p className='font-poppins font-medium text-[12px] 
+        text-[#AFB0C0]'>Friends</p>
+        </div>
+    
+        
+      </div>
+      <div className="">
+        <div className=" hover:bg-[#23D2E2] hover:text-white  text-[#AFB0C0]  py-[15px] p-[20px] flex items-center gap-7 rounded-[10px]">
+          <FaNewspaper />
+          <p className='font-poppins font-semibold text-[14px] 
+        '>Newsfeed</p>
+
+        </div>
+      </div>
+     </div>
+    </div>
+    </>
   )
 }
 
